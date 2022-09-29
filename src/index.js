d16375543e6367b7ff37bde4924e80111a1ca2c3
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-09-29 15:24:30 +0300

    Імпортує бібліотеки до проекту

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+//    <input type="text" id="search-box" />
+//     <ul class="country-list"></ul>
+//     <div class="country-info"></div>
+
+const inpurRef = document.querySelector('#search-box');
+const listRef = document.querySelector('.country-list');
+const infoRef = document.querySelector('.country-info');
+
+// ---
+// if (country > 10) {
+//     Notiflix.Notify.info(
+//       'Too many matches found. Please enter a more specific name.'
+//     );
+// }
+
+// if (country >= 2 && country <= 10) {
+//     // list country
+// }
+
+// if (!country) {
+//   return Notify.failure('Oops, there is no country with that name');
+// }
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     // Data handling
+//   })
+//   .catch(error => {
+//     // Error handling
+//   });
