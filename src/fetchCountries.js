40a96361989ee5f7e7e7f41323e0adce7fd3a8cb
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-09-29 15:25:11 +0300

    Create fetchCountries.js

---
+++ src/fetchCountries.js
@@ -0,0 +1,11 @@
+export function fetchCountries(name) {
+    // body
+}
+
+
+
+// name.official - повна назва країни
+// capital - столиця
+// population - населення
+// flags.svg - посилання на зображення прапора
+// languages - масив мов
