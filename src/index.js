d61e3ded714c701f93ad641ce7c262b726a823d2
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-09-29 15:25:41 +0300

    Імпортує fetchCountries.js до проекту

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
